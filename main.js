a721ae0d4288d7f1f4c69e9ede2764205d37902f
wellsmaciel <103363313+wellsmaciel@users.noreply.github.com>
2026-03-14 17:20:22 +0000

    HTML, CSS and JS created

---
+++ main.js
@@ -0,0 +1,20 @@
+document.getElementById('submit-btn').addEventListener('click', function (event) {
+  event.preventDefault();
+  const shop_item = document.getElementById('item').value;
+  const qtd = document.getElementById('quantity').value;
+  if (!shop_item || !qtd) {
+    alert('Please fill both fields.');
+  } else {
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const text = document.createTextNode(`${shop_item} - Quantity: ${qtd}`);
+    const list = document.getElementById('result');
+    checkbox.type = 'checkbox';
+    listItem.appendChild(checkbox);
+    listItem.appendChild(text);
+    list.appendChild(listItem);
+
+    document.getElementById('item').value = '';
+    document.getElementById('quantity').value = '';
+  }
+});
